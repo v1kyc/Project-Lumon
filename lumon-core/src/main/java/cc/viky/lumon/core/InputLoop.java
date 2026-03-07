1d442b6f489efee816244d0d9c145f9a9525fe7f
ViciousVikk <emaniuel@proton.me>
2026-03-07 18:24:26 +0100

    todo: add InputLoop stub

--- lumon-core/src/main/java/cc/viky/lumon/core/InputLoop.java
+++ lumon-core/src/main/java/cc/viky/lumon/core/InputLoop.java
@@ -1,5 +1,9 @@
 package cc.viky.lumon.core;
 
 public class InputLoop {
-
+    // TODO(inputloop): Implement InputLoop.java
+    // - Single virtual thread (Thread.ofVirtual()) blocking on System.in
+    // - Parse raw key bytes into KeyEvent objects
+    // - Dispatch KeyEvents to App for state mutation
+    // - Handle Ctrl+C explicitly since raw mode disables OS interception
 }
